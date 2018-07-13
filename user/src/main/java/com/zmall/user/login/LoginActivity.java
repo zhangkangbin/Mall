4451cb0c457ba52119e3a286f9970c27fb0f3a0e
张康斌 <zhangkb@my089.net>
2018-07-13 15:32:05 +0800

    添加用户相关

---
+++ user/src/main/java/com/zmall/user/login/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.zmall.user.login;
+import android.os.Bundle;
+import android.view.View;
+
+import com.z.baselibrary.ui.BaseAppCompatActivity;
+import com.zmall.user.R;
+
+public class LoginActivity extends BaseAppCompatActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void click(View view) {
+
+    }
+}
---
+++ user/src/main/java/com/zmall/user/register/UserRegisterActivity.java
@@ -0,0 +1,32 @@
+package com.zmall.user.register;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.z.baselibrary.ui.BaseAppCompatActivity;
+import com.zmall.user.R;
+
+public class UserRegisterActivity extends BaseAppCompatActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_user_register;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void click(View view) {
+
+    }
+
+}
